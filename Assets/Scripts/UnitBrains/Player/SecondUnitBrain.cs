4127149c07e9d1c817ed662461236809b0e1c2cd
importage <sevastyanov.cs@gmail.com>
2024-07-10 21:45:29 +0300

    change GenerateProjectiles logic

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -16,12 +16,18 @@ namespace UnitBrains.Player
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
+
+            if (GetTemperature() >= OverheatTemperature) { return; }
             ///////////////////////////////////////
             // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
             ///////////////////////////////////////
+            for (int i = 0; i < _temperature; i++)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
+            ///////////////////////////////////////
+            IncreaseTemperature();
         }
 
         public override Vector2Int GetNextStep()
